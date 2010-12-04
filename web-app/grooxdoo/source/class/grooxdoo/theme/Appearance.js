25ecd6411e72e1227a0dd0315c379847f08dade7
mengu <mengukagan@gmail.com>
2010-12-05 01:42:36 +0200

    added the generated qooxdoo application.

---
+++ web-app/grooxdoo/source/class/grooxdoo/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("grooxdoo.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
---
+++ web-app/grooxdoo/source/class/grooxdoo/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("grooxdoo.theme.Theme",
+{
+  meta :
+  {
+    color : grooxdoo.theme.Color,
+    decoration : grooxdoo.theme.Decoration,
+    font : grooxdoo.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : grooxdoo.theme.Appearance
+  }
+});
